d898bcf78f9565e3c7823a04eddca91f623c68a2
pascalcpp <1287971058@qq.com>
2021-03-26 04:11:30 +0800

    2021-03-26

--- src/com/xpcf/algorithm/P3.java
+++ src/com/xpcf/algorithm/P3.java
@@ -28,42 +28,24 @@ public class P3 {
 
     class Solution {
         public int lengthOfLongestSubstring(String s) {
-            char[] charArray = s.toCharArray();
-            int length = s.length();
-            int index = 0;
-            int curMaxLength, curStart, curEnd, endLength;
-            curMaxLength = curStart = curEnd = 0;
-            if (s == null || s.length() == 0) {
-                return 0;
-            }
-
-            HashSet<Object> set = new HashSet<>();
-
-            while (index < length) {
-                char ch = charArray[index];
-                if (!set.contains(ch)) {
-                    set.add(ch);
-                    curEnd = index;
-                } else {
-
-                    if (curEnd - curStart > curMaxLength - 1) {
-                        curMaxLength = curEnd - curStart + 1;
-                    }
-
-                    index = curEnd;
-                    set.remove(charArray[curStart]);
-                    ++curStart;
+            Set<Character> occ = new HashSet<Character>();
+            int n = s.length();
+            // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+            int rk = -1, ans = 0;
+            for (int i = 0; i < n; ++i) {
+                if (i != 0) {
+                    // 左指针向右移动一格，移除一个字符
+                    occ.remove(s.charAt(i - 1));
                 }
-
-                ++index;
+                while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                    // 不断地移动右指针
+                    occ.add(s.charAt(rk + 1));
+                    ++rk;
+                }
+                // 第 i 到 rk 个字符是一个极长的无重复字符子串
+                ans = Math.max(ans, rk - i + 1);
             }
-
-
-
-
-            endLength = curEnd - curStart + 1;
-
-            return curMaxLength < endLength ? endLength : curMaxLength;
+            return ans;
         }
     }
 }
---
+++ src/com/xpcf/algorithm/P83.java
@@ -0,0 +1,74 @@
+package com.xpcf.algorithm;
+
+/**
+ *
+ * 执行用时：
+ * 0 ms
+ * , 在所有 Java 提交中击败了
+ * 100.00%
+ * 的用户
+ * 内存消耗：
+ * 38 MB
+ * , 在所有 Java 提交中击败了
+ * 33.74%
+ * 的用户
+ * @author XPCF
+ * @version 1.0
+ * @date 3/26/2021 3:22 AM
+ */
+public class P83 {
+    public static void main(String[] args) {
+        P83 p83 = new P83();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+        p83.deleteDuplicates(head);
+
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode next, lNode, dummyNode = new ListNode(1024, head);
+        next = dummyNode;
+
+        while (next.next != null) {
+            lNode = next;
+            next = next.next;
+
+            if (lNode.val == next.val) {
+                while (lNode.val == next.val) {
+                    next = next.next;
+                    if (next == null) {
+                        lNode.next = null;
+                        return dummyNode.next;
+                    }
+                }
+                lNode.next = next;
+
+            }
+
+
+        }
+
+        return dummyNode.next;
+
+    }
+}
