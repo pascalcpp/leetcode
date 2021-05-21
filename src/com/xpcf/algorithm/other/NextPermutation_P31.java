bfca69dd32fb8915dec129cfb6f809f746f6911c
pascalcpp <1287971058@qq.com>
2021-05-22 03:16:47 +0800

    2021-05-22

---
+++ src/com/xpcf/algorithm/other/NextPermutation_P31.java
@@ -0,0 +1,52 @@
+package com.xpcf.algorithm.other;
+
+import java.util.Arrays;
+
+/**
+ * @author XPCF
+ * @version 1.0
+ * @date 5/21/2021 6:27 PM
+ */
+public class NextPermutation_P31 {
+
+    public static void main(String[] args) {
+        int[] ints = {2, 4, 6};
+        Arrays.sort(ints);
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        while (i > -1 && nums[i] >= nums[i + 1]) {
+            --i;
+        }
+
+        if (i > -1) {
+            int j = nums.length - 1;
+            while (nums[i] >= nums[j]) {
+                --j;
+            }
+            // swap
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        //reverse
+        ++i;
+        int j = nums.length - 1;
+        int temp;
+        while (i < j) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            --j;
+            ++i;
+        }
+    }
+
+
+}
